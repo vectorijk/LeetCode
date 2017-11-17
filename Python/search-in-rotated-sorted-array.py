b55a50ade304bb593ba3a03bad06458be3216863
Kai Jiang <jiangkai@gmail.com>
2017-11-17 04:51:51 -0800

    Update search-in-rotated-sorted-array.py

--- Python/search-in-rotated-sorted-array.py
+++ Python/search-in-rotated-sorted-array.py
@@ -17,6 +17,32 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        
+        left, right = 0, len(nums)-1
+        
+        while left <= right:
+            mid = (left+right) // 2  # (start + end) << 1
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < nums[right]:
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+                
+        return -1
+    
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         left, right = 0, len(nums) - 1
         
         while left <= right:
@@ -31,6 +57,8 @@ class Solution(object):
                 left = mid + 1
 
         return -1
+    
+    
         
 
 if __name__ == "__main__":
