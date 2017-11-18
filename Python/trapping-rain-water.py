e35770e1db1fc57f67841969d76d579a176222a4
Kai Jiang <jiangkai@gmail.com>
2017-11-18 09:52:26 -0800

    Update trapping-rain-water.py

--- Python/trapping-rain-water.py
+++ Python/trapping-rain-water.py
@@ -17,6 +17,34 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        if len(height) == 0:
+            return 0
+        
+        left_max = [0] * len(height)
+        right_max = [0] * len(height)
+        
+        left_max[0] = height[0]
+        for i in range(1, len(height)):
+            left_max[i] = max(left_max[i-1], height[i-1])
+            
+        right_max[-1] = height[-1]
+        for i in range(len(height)-2, -1, -1):
+            right_max[i] = max(right_max[i+1], height[i+1])
+            
+        
+        ans = 0
+        for i in range(len(height)):
+            if min(left_max[i], right_max[i]) > height[i]:
+                ans += min(left_max[i], right_max[i]) - height[i]
+            
+        return ans
+
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
         max_left = max_right = ans = 0
         L, R = 0, len(height) - 1
         while L < R:
