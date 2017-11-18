889dfab2ff48616d538522647843a78a3889871b
Kai Jiang <jiangkai@gmail.com>
2017-11-18 03:35:48 -0800

    Update lowest-common-ancestor-of-a-binary-tree.py

--- Python/lowest-common-ancestor-of-a-binary-tree.py
+++ Python/lowest-common-ancestor-of-a-binary-tree.py
@@ -28,6 +28,29 @@
 #         self.right = None
 
 class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+        if root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        if left:
+            return left
+        if right:
+            return right
+    
+    
     # @param {TreeNode} root
     # @param {TreeNode} p
     # @param {TreeNode} q
