58139a01396c3e2722e6436b51d4e275ec4a49bf
Kai Jiang <jiangkai@gmail.com>
2017-11-21 01:38:58 -0800

    Update remove-invalid-parentheses.py

--- Python/remove-invalid-parentheses.py
+++ Python/remove-invalid-parentheses.py
@@ -13,6 +13,30 @@
 # ")(" -> [""]
 #
 
+# BFS 
+class Solution(object):
+    def removeInvalidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        if not s: return ['']
+        q = {s}
+        while q:
+            ans = filter(self.isValidParentheses,q)
+            if ans: return ans
+            q = {cur[:i] + cur[i + 1:] for cur in q for i in xrange(len(cur))}
+
+    def isValidParentheses(self, s):
+        cnt = 0
+        for c in s:
+            if c == '(':
+                cnt += 1
+            elif c == ')':
+                if cnt == 0: return False
+                cnt -= 1
+        return cnt == 0
+
 # DFS solution.
 class Solution(object):
     def removeInvalidParentheses(self, s):
