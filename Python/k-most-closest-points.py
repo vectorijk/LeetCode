1a5226db991dfd65a0e2ba3ff3efc45a8e663593
Kai Jiang <jiangkai@gmail.com>
2017-11-22 02:03:18 -0800

    Create k-most-closest-points.py

---
+++ Python/k-most-closest-points.py
@@ -0,0 +1,32 @@
+import heapq
+
+class point(object):
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+	def __lt__(self, other):
+		return self.x**2 + self.y**2 > other.x**2 + other.y**2
+	def __eq__(self, other):
+		return self.x**2 + self.y**2 == other.x**2 + other.y**2
+	def __str__(self):
+		return '('+str(self.x)+', '+str(self.y)+')'
+
+min_heap = []
+
+k = 4
+
+points = [point(1,2), point(4,5), point(7,8), point(2,3), point(3,2),
+point(3,4)]
+
+print points[0] < points[1]
+
+for p in points:
+	heapq.heappush(min_heap, p)
+	if len(min_heap) == k + 1:
+		heapq.heappop(min_heap)
+
+result = []
+while min_heap:
+	result.append(heapq.heappop(min_heap))
+
+print map(str, result)[::-1]
