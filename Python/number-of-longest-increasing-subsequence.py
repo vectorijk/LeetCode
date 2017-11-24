8aae6a87cc79d5bd009609c0ec6317b57ac4792e
Kai Jiang <jiangkai@gmail.com>
2017-11-24 02:31:06 -0800

    Update number-of-longest-increasing-subsequence.py

--- Python/number-of-longest-increasing-subsequence.py
+++ Python/number-of-longest-increasing-subsequence.py
@@ -14,6 +14,26 @@
 # 5 subsequences' length is 1, so output 5.
 # Note: Length of the given array will be not exceed 2000 and the answer is guaranteed
 # to be fit in 32-bit signed int.
+
+
+class Solution(object):
+    def findNumberOfLIS(self, nums):
+        N = len(nums)
+        if N <= 1: return N
+        lengths = [0] * N #lengths[i] = longest ending in nums[i]
+        counts = [1] * N #count[i] = number of longest ending in nums[i]
+
+        for j, num in enumerate(nums):
+            for i in xrange(j):
+                if nums[i] < nums[j]:
+                    if lengths[i] >= lengths[j]:
+                        lengths[j] = 1 + lengths[i]
+                        counts[j] = counts[i]
+                    elif lengths[i] + 1 == lengths[j]:
+                        counts[j] += counts[i]
+
+        longest = max(lengths)
+        return sum(c for i, c in enumerate(counts) if lengths[i] == longest)
     
 class Solution(object):
     def findNumberOfLIS(self, nums):
