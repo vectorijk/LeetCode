5a1d40dff45e030ca1647a59ed08852288740eba
Kai Jiang <jiangkai@gmail.com>
2017-11-24 04:58:17 -0800

    Update island-perimeter.py

--- Python/island-perimeter.py
+++ Python/island-perimeter.py
@@ -21,6 +21,35 @@
 # Answer: 16
 import operator
 
+#my solution
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        n = len(grid)
+        if n == 0: return 0
+        m = len(grid[0])
+        if m == 0: return 0
+        
+        
+        cnt = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 0:
+                    continue
+                    
+                if i == 0 or grid[i-1][j] == 0:
+                    cnt += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    cnt += 1
+                if i == n - 1 or grid[i+1][j] == 0:
+                    cnt += 1
+                if j == m - 1 or grid[i][j+1] == 0:
+                    cnt += 1
+                    
+        return cnt
 
 class Solution(object):
     def islandPerimeter(self, grid):
