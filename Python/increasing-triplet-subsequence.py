aec6d8818cae2846a7373460178a5e59d7073c92
Kai Jiang <jiangkai@gmail.com>
2017-11-24 04:31:08 -0800

    Update increasing-triplet-subsequence.py

--- Python/increasing-triplet-subsequence.py
+++ Python/increasing-triplet-subsequence.py
@@ -18,6 +18,25 @@
 # return false.
 
 
+#这个思路是使用两个指针m1和m2，初始化为整型最大值，我们遍历数组，如果m1大于等于当前数字，则将当前数字赋给m1
+#如果m1小于当前数字且m2大于等于当前数字，那么将当前数字赋给m2，一旦m2被更新了，说明一定会有一个数小于m2,
+#那么我们就成功的组成了一个长度为2的递增子序列，所以我们一旦遍历到比m2还大的数，我们直接返回ture。
+#如果我们遇到比m1小的数，还是要更新m1，有可能的话也要更新m2为更小的值，毕竟m2的值越小，能组成长度为3的递增序列的可能性越大，参见代码如下：
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int m1 = INT_MAX, m2 = INT_MAX;
+        for (auto a : nums) {
+            if (a <= m1) m1 = a;
+            else if (a <= m2) m2 = a;
+            else return true;   # a > m2 > m1
+        }
+        return false;
+    }
+};
+
+
 class Solution(object):
     def increasingTriplet(self, nums):
         """
