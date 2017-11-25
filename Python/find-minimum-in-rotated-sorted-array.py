bbb20285833bbb1f27eb1f53d7e247db72c10224
Kai Jiang <jiangkai@gmail.com>
2017-11-25 06:36:33 -0800

    Update find-minimum-in-rotated-sorted-array.py

--- Python/find-minimum-in-rotated-sorted-array.py
+++ Python/find-minimum-in-rotated-sorted-array.py
@@ -47,6 +47,27 @@ class Solution2(object):
 
         return nums[left]
 
+class Solution {
+public:
+    int findMin(vector<int> &num) {
+        return findMin(num, 0, num.size()-1);
+    }
+    
+private:
+    int findMin(const vector<int>& num, int l, int r)
+    {
+        // Only 1 or 2 elements
+        if (l+1 >= r) return min(num[l], num[r]);
+        
+        // Sorted
+        if (num[l] < num[r]) return num[l];
+        
+        int mid = l + (r-l)/2; 
+        
+        return min(findMin(num, l, mid-1), 
+                   findMin(num, mid, r));
+    }
+};
 
 if __name__ == "__main__":
     print Solution().findMin([1])
