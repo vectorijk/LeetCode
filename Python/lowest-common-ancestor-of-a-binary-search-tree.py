65f110edc22528e8a5be5647d9a446f3e91aa235
Kai Jiang <jiangkai@gmail.com>
2017-11-26 19:31:51 -0800

    Update lowest-common-ancestor-of-a-binary-search-tree.py

--- Python/lowest-common-ancestor-of-a-binary-search-tree.py
+++ Python/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -38,3 +38,19 @@ class Solution:
             root = root.left if s <= root.val else root.right
         # s <= root.val <= b.
         return root
+
+    
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:return root;
+        if p.val > q.val:return self.lowestCommonAncestor(root, q, p)
+        if root.val >= p.val and root.val <= q.val: return root;
+        if root.val < p.val:return self.lowestCommonAncestor(root.right, p, q)
+        if root.val > q.val:return self.lowestCommonAncestor(root.left, p, q)
