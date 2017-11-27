83ccf1fb92e0492c48164649fe4c536968776513
Kai Jiang <jiangkai@gmail.com>
2017-11-26 21:53:58 -0800

    Update flatten-binary-tree-to-linked-list.py

--- Python/flatten-binary-tree-to-linked-list.py
+++ Python/flatten-binary-tree-to-linked-list.py
@@ -90,3 +90,28 @@ if __name__ == "__main__":
     print result.right.right.right.val
     print result.right.right.right.right.val
     print result.right.right.right.right.right.val
+
+    
+    
+    
+# class Solution {
+# public:
+#     void flatten(TreeNode *root) {
+#         if(!root) return;
+#         vector<TreeNode*> allNodes;
+#         preorder(root, allNodes);
+#         int n = allNodes.size();
+#         for(int i=0; i<n-1; i++) {
+#             allNodes[i]->left = NULL;
+#             allNodes[i]->right = allNodes[i+1];
+#         }
+#         allNodes[n-1]->left = allNodes[n-1]->right = NULL;
+#     }
+    
+#     void preorder(TreeNode *root, vector<TreeNode*> &allNodes) {
+#         if(!root) return;
+#         allNodes.push_back(root);
+#         preorder(root->left, allNodes);
+#         preorder(root->right, allNodes);
+#     }
+# };
