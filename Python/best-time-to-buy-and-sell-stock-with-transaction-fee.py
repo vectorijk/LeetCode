8da4b56d542bbba381cc76a1cfe98c68fbc51f60
Kai Jiang <jiangkai@gmail.com>
2017-11-27 02:01:19 -0800

    Update best-time-to-buy-and-sell-stock-with-transaction-fee.py

--- Python/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ Python/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -27,6 +27,17 @@
 # - 0 < prices[i] < 50000.
 # - 0 <= fee < 50000.
 
+
+# At the end of the i-th day, we maintain cash, the maximum profit we could have if we did not have a share of stock,
+# and hold, the maximum profit we could have if we owned a share of stock.
+
+# To transition from the i-th day to the i+1-th day, we either sell our stock 
+#cash = max(cash, hold + prices[i] - fee) or 
+#buy a stock hold = max(hold, cash - prices[i]). 
+#At the end, we want to return cash. We can transform cash first without using temporary 
+#variables because selling and buying on the same day can't be better than just continuing to hold the stock.
+
+
 class Solution(object):
     def maxProfit(self, prices, fee):
         """
