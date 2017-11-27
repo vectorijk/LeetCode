d126d08217b3212e1862cbac3d7ecb130321c25d
Kai Jiang <jiangkai@gmail.com>
2017-11-26 21:31:04 -0800

    Update binary-search-tree-iterator.py

--- Python/binary-search-tree-iterator.py
+++ Python/binary-search-tree-iterator.py
@@ -10,6 +10,30 @@
 # and uses O(h) memory, where h is the height of the tree.
 #
 
+class BSTIterator:
+    # @param root, a binary search tree's root node
+    def __init__(self, root):
+        self.q=[]
+        self.allLeftIntoStack(root)
+ 
+    # @return a boolean, whether we have a next smallest number
+    def hasNext(self):
+        if not self.q:return False
+        return True
+ 
+    # @return an integer, the next smallest number
+    def next(self):
+        cur = self.q.pop()
+        self.allLeftIntoStack(cur.right)
+        return cur.val
+ 
+    def allLeftIntoStack(self,root):
+        while root:
+            self.q.append(root)
+            root=root.left
+
+
+
 # Definition for a  binary tree node
 class TreeNode:
     def __init__(self, x):
@@ -48,3 +72,6 @@ if __name__ == "__main__":
     while i.hasNext(): v.append(i.next())
     
     print v
+
+    
+    
