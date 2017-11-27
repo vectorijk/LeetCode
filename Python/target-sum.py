c881b60f4a86a012beceaaab82fcb5891d7aa5a3
Kai Jiang <jiangkai@gmail.com>
2017-11-26 18:47:09 -0800

    Update target-sum.py

--- Python/target-sum.py
+++ Python/target-sum.py
@@ -44,3 +44,22 @@ class Solution(object):
         if total < S or (S + total) % 2: return 0
         P = (S + total) // 2
         return subsetSum(nums, P)
+
+    
+    
+    class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int res = 0;
+        helper(nums, S, 0, res);
+        return res;
+    }
+    void helper(vector<int>& nums, int S, int start, int& res) {
+        if (start >= nums.size()) {
+            if (S == 0) ++res;
+            return;
+        }
+        helper(nums, S - nums[start], start + 1, res);
+        helper(nums, S + nums[start], start + 1, res);
+    }
+};
